b5abdf053c24abfb4b4e6d1a8137d342c0fd71d2
SiddheshHirulkar <siddhu3096@gmail.com>
2020-12-25 20:33:46 +0530

    Updates

---
+++ precache-manifest.1a4fbb85179f6e5b4dab348a0326afd8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07cddc9a95fb1c905e8ff3e8669f13e6",
+    "url": "/React-To-Do-List/index.html"
+  },
+  {
+    "revision": "dd3d5ec3c8ccff194034",
+    "url": "/React-To-Do-List/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "ff2410033052fcae177f",
+    "url": "/React-To-Do-List/static/css/main.85fd4d38.chunk.css"
+  },
+  {
+    "revision": "dd3d5ec3c8ccff194034",
+    "url": "/React-To-Do-List/static/js/2.535c0869.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/React-To-Do-List/static/js/2.535c0869.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff2410033052fcae177f",
+    "url": "/React-To-Do-List/static/js/main.44a11fc0.chunk.js"
+  },
+  {
+    "revision": "bec5ca349772c839d9af",
+    "url": "/React-To-Do-List/static/js/runtime-main.42153f78.js"
+  }
+]);
